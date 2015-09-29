a3376663bf14c2ac1ec1ff984558a6b8c5d8d1c3
humbertolopez <humberto@373.mx>
2015-09-29 13:35:01 -0500

    Martes de echarle los kilos
    O algo así.

--- header.php
+++ header.php
@@ -18,47 +18,5 @@
 	<?php get_template_part('info-contacto'); ?>
 	<!-- /contacto -->
 	<!-- header -->
-	<header>
-		<div id="nav">
-			<a href="#portada"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg"></a>
-			<ul id="menu">
-				<li class="menu-item">
-					<a href="#eventos">Productos para Eventos</a>
-					<ul class="sub-menu">
-						<li class="sub-menu-item"><a href="">Remolques con Sanitarios</a></li>
-						<li class="sub-menu-item"><a href="">Sanitarios Presidenciales</a></li>
-						<li class="sub-menu-item"><a href="">Camerinos</a></li>
-						<li class="sub-menu-item"><a href="">Sanitarios Portátiles</a></li>
-					</ul>
-				</li>
-				<li class="menu-item">
-					<a href="#industriales">Productos Industriales</a>
-					<ul class="sub-menu">
-						<li class="sub-menu-item"><a href="">Sanitario Industrial Individual</a></li>
-						<li class="sub-menu-item"><a href="">Sanitario Industrial Doble</a></li>
-						<li class="sub-menu-item"><a href="">Oficinas Móviles</a></li>
-						<li class="sub-menu-item"><a href="">Dormitorios</a></li>
-						<li class="sub-menu-item"><a href="">Centros de Lavado</a></li>
-						<li class="sub-menu-item"><a href="">Baños para Uso Rudo</a></li>
-						<li class="sub-menu-item"><a href="">Regaderas Móviles</a></li>
-					</ul>
-				</li>
-				<li class="menu-item">
-					<a href="">Proyectos Especiales</a>
-					<ul class="sub-menu">
-						<li class="sub-menu-item"><a href="">Remolques con Publicidad</a></li>
-						<li class="sub-menu-item"><a href="">Construcciones Especiales</a></li>
-						<li class="sub-menu-item"><a href="">Baños Urbanos</a></li>
-						<li class="sub-menu-item"><a href="">Servicios Sanitarios en Casetas de Cobro</a></li>
-					</ul>
-				</li>
-				<li class="menu-item">
-					<a href="">La Empresa</a>
-				</li>
-				<li class="menu-item">
-					<a href="">Contacto</a>
-				</li>
-			</ul>
-		</div>
-	</header>
+	<?php get_template_part('header-part'); ?>
 	<!-- /header -->
--- index.php
+++ index.php
@@ -5,46 +5,36 @@
 			<a href="#industriales"><img src="<?php echo get_template_directory_uri(); ?>/img/industrial.jpg"></a>
 			<div class="inner-portada">
 				<h1>Productos Industriales Escumovil®</h1>
-				<p>Este es un hermoso párrafo que sirve como descripción para los productos industriales de Escumovil®.</p>
+				<p>Sanitarios móviles y portátiles para construcción, minería y la industria petrolera.</p>
 			</div>
 		</div>
 		<div class="eventos">
 			<a href="#eventos"><img src="<?php echo get_template_directory_uri(); ?>/img/eventos.jpg"></a>
 			<div class="inner-portada">
 				<h1>Productos Escumovil® para Eventos</h1>
-				<p>Este es un hermoso párrafo que sirve como descripción para los productos Escumovil® para eventos.</p>
+				<p>Remolques con sanitarios, baños móviles y portátiles para eventos sociales, masivos y deportivos.</p>
 			</div>
 		</div>
 		<div class="especiales">
 			<a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/especiales.jpg"></a>
 			<div class="inner-portada">
 				<h1>Proyectos Especiales</h1>
-				<p>Este es un hermoso párrafo que sirve como descripción para los proyectos especiales hechos por Escumovil®.</p>
+				<p>Creaciones únicas a la medida, ideas para tu próximo negocio.</p>
 			</div>
 		</div>
 		<div class="empresa">
 			<a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/empresa.jpg"></a>
 			<div class="inner-portada">
 				<h1>La Empresa</h1>
-				<p>Este es un hermoso párrafo que sirve como descripción para la empresa, Escumovil®.</p>
+				<p>Historia, valores y logros de nuestra organización.</p>
 			</div>
 		</div>
 	</section>
 	<!-- /portada -->
 	
-	<!-- industriales -->
-	<section id="industriales" class="block">
-		<div class="inner block">
-			<div class="content">
-				<h1>Productos Industriales Escumovil®</h1>
-				<p>Este es un hermoso párrafo que sirve como descripción para los productos industriales de Escumovil®.</p>
-			</div>
-		</div>
-	</section>
-	<!-- industriales -->
-
 	<!-- eventos -->
 	<section id="eventos" class="block">
+		<div class="bg"></div>
 		<div class="inner block">
 			<div class="content">
 				<h1>Productos Escumovil® para Eventos</h1>
@@ -52,6 +42,42 @@
 			</div>
 		</div>
 	</section>
-	<!-- eventos -->
+	<!-- /eventos -->
+
+	<!-- industriales -->
+	<section id="industriales" class="block">
+		<div class="bg"></div>
+		<div class="inner block">
+			<div class="content">
+				<h1>Productos Industriales Escumovil®</h1>
+				<p>Este es un hermoso párrafo que sirve como descripción para los productos industriales de Escumovil®.</p>
+			</div>
+		</div>
+	</section>
+	<!-- /industriales -->
+
+	<!-- especiales -->
+	<section id="especiales" class="block">
+		<div class="bg"></div>
+		<div class="inner block">
+			<div class="content">
+				<h1>Proyectos Especiales Escumovil®</h1>
+				<p>Este es un hermoso párrafo que sirve como descripción para los productos especiales de Escumovil®.</p>
+			</div>
+		</div>
+	</section>
+	<!-- /especiales -->
+
+	<!-- empresa -->
+	<section id="empresa" class="block">
+		<div class="bg"></div>
+		<div class="inner block">
+			<div class="content">
+				<h1>La Empresa</h1>
+				<p>Este es un hermoso párrafo que sirve como descripción para los productos empresa de Escumovil®.</p>
+			</div>
+		</div>
+	</section>
+	<!-- /empresa -->
 
 <?php get_footer(); ?>
