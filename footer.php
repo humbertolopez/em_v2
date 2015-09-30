8b118d36be3080eba6d907e4debfa0e3d4609b27
humbertolopez <humberto@373.mx>
2015-09-29 19:11:06 -0500

    09292015

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
 	<footer>
+		<div class="innerproducto">
+			<div class="cuarto">
+				<h3>Escumovil®, servicios de clase mundial.</h3>
+			</div>
+			<div class="cuarto">
+				<h3>Productos Escumovil® para Eventos.</h3>
+			</div>
+			<div class="cuarto">
+				<h3>Productos Industriales Escumovil®.</h3>
+			</div>
+		</div>
 	</footer>
 
 </body>
